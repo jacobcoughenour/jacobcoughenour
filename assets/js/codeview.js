6acbcc5b5845157c2cc66147f7de827886d45b52
Jacob Coughenour <me@jacobcoughenour.com>
2025-07-14 21:45:59 -0400

    stuff

--- assets/js/codeview.js
+++ assets/js/codeview.js
@@ -1,32 +1,90 @@
 // improvements to the existing code highlighting
 
+import dmpModule from "./diff_match_patch_uncompressed.js";
+
+
 function init() {
 	const views = [];
 	const idMap = {};
 
 	const elements = document.getElementsByClassName("codeview");
-	[...views].forEach(/** @type {Element} */ view => {
+	if (elements.length === 0)
+		return;
+
+	var dmp = new dmpModule.diff_match_patch();
+
+	[...elements].forEach(/** @type {Element} */ view => {
 
 		const params = JSON.parse(view.querySelector("script#params").innerHTML);
 		const raw = decodeHtmlEntity(view.querySelector("script#code").innerHTML);
 
-		// const view = {
-		// 	element: view,
-		// 	params,
-		// 	raw,
+		// copy button
+		const copyButton = view.getElementsByClassName("copy-button")[0];
+		copyButton.addEventListener("click", () => {
+			copyTextToClipboard(raw);
+		});
+
+		const codeElement = view.querySelector("code");
+		const lineElements = [...codeElement.children];
+
+		const data = {
+			element: view,
+			params,
+			raw,
+			copyButton,
+			lineElements
+		};
+
+		views.push(data);
+		if (params?.id) {
+			idMap[params?.id] = data;
+		}
+	});
+
+	function diff_lineMode(text1, text2) {
+		var a = dmp.diff_linesToChars_(text1, text2);
+		var lineText1 = a.chars1;
+		var lineText2 = a.chars2;
+		var lineArray = a.lineArray;
+		var diffs = dmp.diff_main(lineText1, lineText2, false);
+		dmp.diff_charsToLines_(diffs, lineArray);
+		return diffs;
+	}
+	
+	views.forEach(view => {
+		if (!view.params.diff || !idMap[view.params.diff]) {
+			return;
+		}
+		view.diff = diff_lineMode(idMap[view.params.diff].raw, view.raw);
+		console.log(view.diff);
+
+		// var curSpan = 0;
+		// for (var i = 0; i < view.diff.length && curSpan < view.lineElements.length; i++) {
+		// 	const span = view.lineElements[curSpan];
+		// 	console.log(span.innerHTML)
+
 		// }
+	});
 
-		views.push(view);
+	function collapseDiff(view) {
+		if (!view.diff)
+			return;
+		console.log(view);
 
-		// // copy button
-		// const copy_button = view.getElementsByClassName("copy-button")[0];
-		// copy_button.addEventListener("click", () => {
-		// 	console.log("click");
+		const windowLines = 1;
 
-		// 	copyTextToClipboard(content);
+		view.lineElements.forEach((e, i) => {
+			var diffLine = view.diff[i];
 
+			// e.style.display = "none";
 
-		// });
+		});
+	}
+
+	views.forEach(view => {
+		if (view.diff) {
+			collapseDiff(view);
+		}
 	});
 }
 
